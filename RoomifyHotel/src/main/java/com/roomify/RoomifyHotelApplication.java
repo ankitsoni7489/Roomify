b3eeb68272fce7d6aedb8e5da612c7bcbe5bce68
ankitsoni7489 <82995707+ankitsoni7489@users.noreply.github.com>
2025-02-17 22:51:24 +0530

    Connect

---
+++ RoomifyHotel/src/main/java/com/roomify/RoomifyHotelApplication.java
@@ -0,0 +1,13 @@
+package com.roomify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoomifyHotelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoomifyHotelApplication.class, args);
+	}
+
+}
